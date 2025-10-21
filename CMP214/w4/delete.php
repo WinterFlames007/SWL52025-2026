2c5ac7b7e691bdac26e58580ec6468ce81a83ecf
Prince Ogbeide <ogbeidprince80@gmail.com>
2025-10-21 11:49:45 +0100

    Delete CMP214/w4 directory

--- CMP214/w4/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("./db.php");
-$sql = "Delete from tblEmp where ID = ".$_GET["eid"]."";
-    $result = $conn->query($sql);
-    echo "<script>alert('Employee is added to the system')</script>";
-    header("Location: ./index.php");
-?>
